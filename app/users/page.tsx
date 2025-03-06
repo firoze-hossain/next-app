26284265f5579f3ce1378e505c4dd737ea803986
firoze-hossain <firoze.hossain@outlook.com>
2025-03-06 20:57:08 +0600

    static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,11 +4,16 @@ interface User {
   name: string;
 }
 export const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users",
+    //  {next:{revalidate:10}});
+    { cache: "no-cache" }
+  );
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
